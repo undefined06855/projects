825a721b95e8f4e6bdef49ba46424a4d1235c512
undefined06855 <98057141+undefined06855@users.noreply.github.com>
2022-11-24 21:26:05 +0000

    fixed my stupid error
    i accidentally added the code for Car

--- main.js
+++ main.js
@@ -1,111 +1,74 @@
-const canvas = document.querySelector("canvas")
-    , ctx = canvas.getContext("2d")
+const req = new XMLHttpRequest()
+var json = {}
 
-
-var started = false
-var keys = {
-    "ArrowLeft": false,
-    "ArrowRight": false
-}
-
-var totalImages = 0
-var loadedImages = 0
-
-class Image2
-{
-    constructor(src, width, height)
+req.onreadystatechange = () => {
+    if (req.readyState == 4 && req.status == 200)
     {
-        totalImages++
+        console.log("Request successful.")
 
-        const image = new Image(width, height)
-        image.src = src
+        json = JSON.parse(req.response)
 
-        image.addEventListener("load", () => {
-            loadedImages++
+        const stylesheet = document.createElement("style")
+
+        var repid = 0
+        json.forEach(repo => {
+            if (!(repo.fork || repo.name === "undefined06855" || repo.name === "projects" || repo.archived || repo.private))
+            {
+                console.log(repo)
+                document.getElementById("repo_wrapper").innerHTML +=
+                reponame
+                .replaceAll("[[TITLE]]", repo.name)
+                .replaceAll("[[DESC]]", repo.description === null ? "(No description provided)" : repo.description)
+                .replaceAll("[[ID]]", repid)
+                .replaceAll("[[URL]]", repo.html_url)
+
+                stylesheet.textContent +=
+                style
+                .replaceAll("[[ID]]", repid)
+                .replaceAll("[[RAND]]", Math.random() * 3 - 1.5)
+                .replaceAll("[[RAND2]]", Math.random() < .5 ? -7 : 7)
+
+                repid++
+            }
+            else {console.log("Declined repo %s", repo.name)}
         })
 
-        this.image = image
-        this.width = width
-        this.height = height
+        document.head.appendChild(stylesheet)
     }
-    draw(x, y, scalex = 1, scaley = 1, rotation = 0)
-    {
-        rotation = rotation * (Math.PI/180)
-        
-        ctx.translate(x, y)
-        ctx.rotate(rotation)
-        ctx.drawImage(this.image, -this.width / 2, -this.height / 2, this.width * scalex, this.height * scaley)
-        ctx.rotate(-rotation)
-        ctx.translate(-x, -y)
-    }
+    else console.log("state changed") 
 }
 
-class Car
-{
-    constructor()
+req.open("get", "https://api.github.com/users/undefined06855/repos")
+req.send()
+
+window.addEventListener("resize", () => {
+    if (document.body.offsetWidth < 450)
     {
-        this.angle = 0
-
-        this.x = canvas.width / 2
-        this.y = canvas.height / 2
-
-        this.image = new Image2("car.png", 48, 48)
+        document.getElementById("repo_wrapper").classList.remove("row")
+        document.getElementById("repo_wrapper").classList.add   ("column")
     }
-    update()
-    {
-        if (keys.ArrowLeft) this.angle -= 2
-        if (keys.ArrowRight) this.angle += 2
-
-        this.x += Math.cos(this.angle * (Math.PI/180)) * 4
-        this.y += Math.sin(this.angle * (Math.PI/180)) * 4
-
-        if (this.x > canvas.width) this.x = canvas.width
-        if (this.x < 0           ) this.x = 0
-        if (this.y > canvas.height) this.y = canvas.height
-        if (this.y < 0           ) this.y = 0
-    }
-    frame()
-    {
-        ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-        if (started)
-        {
-            car.update()
-            car.image.draw(car.x, car.y, 1, 1, car.angle)
-            requestAnimationFrame(car.frame)
-        }
-        else
-        {
-            menuImage.draw(canvas.width / 2, canvas.height / 2)
-            requestAnimationFrame(car.frame)
-        }
-    }
-}
-
-const car = new Car
-const menuImage = new Image2("menu.png", 1920, 1080)
-
-document.addEventListener("keydown", event => {
-    if (!started) started = true
-    if (event.key === "ArrowLeft")  keys.ArrowLeft = true
-    if (event.key === "ArrowRight") keys.ArrowRight = true
-})
-
-document.addEventListener("keyup", event => {
-    if (event.key === "ArrowLeft")  keys.ArrowLeft = false
-    if (event.key === "ArrowRight") keys.ArrowRight = false
-})
-
-
-function waitUntilImagesLoaded()
-{
-    if (loadedImages != totalImages) requestAnimationFrame(waitUntilImagesLoaded)
     else
     {
-        console.log("%s images loaded", loadedImages)
-        requestAnimationFrame(car.frame)
+        document.getElementById("repo_wrapper").classList.add   ("row")
+        document.getElementById("repo_wrapper").classList.remove("column")
     }
+})
+
+if (document.body.offsetWidth < 450)
+{
+    document.getElementById("repo_wrapper").classList.remove("row")
+    document.getElementById("repo_wrapper").classList.add   ("column")
+}
+else
+{
+    document.getElementById("repo_wrapper").classList.add   ("row")
+    document.getElementById("repo_wrapper").classList.remove("column")
 }
 
-requestAnimationFrame(waitUntilImagesLoaded)
+document.getElementById("corner_image").addEventListener("click", () => 
+    window.location.href = "https://github.com/undefined06855"
+)
 
+document.getElementById("corner_click").addEventListener("click", () => 
+    window.location.href = "https://github.com/undefined06855"
+)
